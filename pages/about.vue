ca0994b8a1a1d37cf6bc7a56b81ace4d0023c51b
chrisroselli <chrisroselli@gmail.com>
2020-07-08 22:58:23 -0400

    updated form

--- pages/about.vue
+++ pages/about.vue
@@ -5,17 +5,17 @@
       <div class="w-auto text-center"><p>About Me</p></div>
       <div class="w-64 py-4 mx-5"><hr class="h-line text-black bg-black" /></div>
     </div>
-<div class="flex min-h-screen flex-wrap m-10">
-  <div class="w-full md:w-1/4 px-4">
-    <img src="~/assets/img/profile-pic.jpg" class="w-screen rounded-lg" alt="profile picture">
-  </div>
-  <div class="w-full md:w-3/4 px-2 mt-4 text-center sm:text-left">
-    <p class="text-2xl leading-none">Chris Roselli</p>
-    <p class="text-l text-primary">Web Developer</p>
-    <p class="text-base mt-4">I have been working in web development for over 10 years. I also have a background in graphic design and photographer. I started my journey at Central Connecticut State University with a BA in Graphic/Information Design. My years at Central where some of my favorite. I was fortunate to be offered the opportunity to work within the department, where I gained valuable experience in both design and technology as their lead technician. After graduating, I then held several positions spanning from print to web design. Eventually I landed on web development as my true passion and have been building my skill set in this amazing digital realm ever since.</p>
-    <p class="mt-4">I have been a New Englander all my life and am a resident of Cheshire, CT. I enjoy running, especially on our beautiful Heritage Canal Trail. My family owns a small hobby farm in Colchester, CT raising everything from Nigerian goats to miniature donkeys.</p>
-  </div>
-</div> 
+    <div class="flex min-h-screen flex-wrap m-10">
+      <div class="w-full md:w-1/4 px-4">
+        <img src="~/assets/img/profile-pic.jpg" class="w-screen rounded-lg" alt="profile picture">
+      </div>
+      <div class="w-full md:w-3/4 px-2 mt-4 text-center sm:text-left">
+        <p class="text-2xl leading-none">Chris Roselli</p>
+        <p class="text-l text-primary">Web Developer</p>
+        <p class="text-base mt-4">I have been working in web development for over 10 years. I also have a background in graphic design and photographer. I started my journey at Central Connecticut State University with a BA in Graphic/Information Design. My years at Central where some of my favorite. I was fortunate to be offered the opportunity to work within the department, where I gained valuable experience in both design and technology as their lead technician. After graduating, I then held several positions spanning from print to web design. Eventually I landed on web development as my true passion and have been building my skill set in this amazing digital realm ever since.</p>
+        <p class="mt-4">I have been a New Englander all my life and am a resident of Cheshire, CT. I enjoy running, especially on our beautiful Heritage Canal Trail. My family owns a small hobby farm in Colchester, CT raising everything from Nigerian goats to miniature donkeys.</p>
+      </div>
+    </div> 
   </div>
 </template>
 
--- pages/hireme.vue
+++ pages/hireme.vue
@@ -1,23 +1,72 @@
 <template>
   <div>
-<div class="flex min-h-screen flex-wrap m-10">
-  <div class="flex-auto lg:flex-initial m-auto w-1/2">
-  <p class="uppercase tracking-wide no-underline hover:no-underline font-bold text-gray-800 text-xl mb-8">Hire Me</p>
-    <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-      <div class="mb-4">
-        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Your Name">
+    <div class="flex justify-center pb-2 text-2xl m-10 font-bold uppercase">
+      <div class="w-64 py-4 mx-5"><hr class="h-line text-black bg-black" /></div>
+      <div class="w-auto text-center"><p>Hire Me</p></div>
+      <div class="w-64 py-4 mx-5"><hr class="h-line text-black bg-black" /></div>
+    </div>
+    <div class="flex min-h-screen flex-wrap m-10">
+      <div class="flex-auto lg:flex-initial mx-auto">
+      <form name="contact-form" class="w-full max-w-lg" netlify>
+        <div class="flex flex-wrap -mx-3 mb-4">
+          <div class="w-full md:w-1/2 px-3 mb-4 md:mb-0">
+            <label class="block uppercase tracking-wide text-gray-700 text-sm font-bold mb-2" for="first-name">
+              First Name
+            </label>
+            <input class="appearance-none block w-full bg-footergray text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="first-name" type="text">
+          </div>
+          <div class="w-full md:w-1/2 px-3">
+            <label class="block uppercase tracking-wide text-gray-700 text-sm font-bold mb-2" for="last-name">
+              Last Name
+            </label>
+            <input class="appearance-none block w-full bg-footergray text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="last-name" type="text">
+          </div>
+        </div>
+        <div class="flex flex-wrap -mx-3 mb-4">
+          <div class="w-full px-3">
+            <label class="block uppercase tracking-wide text-gray-700 text-sm font-bold mb-2" for="email">
+              Email
+            </label>
+            <input class="appearance-none block w-full bg-footergray text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="email" type="email">
+          </div>
+        </div>
+        <div class="flex flex-wrap -mx-3 mb-4">
+          <div class="w-full px-3">
+            <label class="block uppercase tracking-wide text-gray-700 text-sm font-bold mb-2" for="company">
+              Company Name
+            </label>
+            <input class="appearance-none block w-full bg-footergray text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="company" type="text">
+          </div>
+        </div>
+        <div class="flex flex-wrap mb-6">
+          <label class="block uppercase tracking-wide text-gray-700 text-sm font-bold mb-2">Which services are you interested in?</label>
+          <div class="w-full">
+              <label class="inline-flex items-center">
+                <input type="checkbox" class="ml-4">
+                <span class="ml-2 text-sm">Web Design</span>
+              </label>
+
+              <label class="inline-flex items-center">
+                <input type="checkbox" class="ml-4">
+                <span class="ml-2 text-sm">Web Development</span>
+              </label>
+
+              <label class="inline-flex items-center">
+                <input type="checkbox" class="ml-4">
+                <span class="ml-2 text-sm">Web Hosting</span>
+              </label>
+          </div>
+        </div>
+        <label class="block uppercase tracking-wide text-gray-700 text-sm font-bold mb-2">
+          <span class="text-gray-700">Please provide a description of your project:</span>
+          <textarea class="form-textarea mt-1 appearance-none block w-full bg-footergray text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" rows="10"></textarea>
+        </label>
+        <button class="bg-primary hover:bg-black text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+        Submit
+      </button>
+      </form>
       </div>
-      <div class="mb-6">
-        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Your Businesses Name">
-      </div>
-      <div class="flex items-center justify-between">
-        <button class="bg-primary hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
-          Submit
-        </button>
-      </div>
-    </form>
-  </div>
-</div> 
+    </div> 
     <Footer />
   </div>
 </template>
